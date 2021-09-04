25667191611b8039f29d363d554d0debf8b2e9b2
Rudrank <rudrankriyam@gmail.com>
2021-09-04 15:20:53 +0530

    Update for iOS 14

--- Sources/RRColorKit/Extensions/String.swift
+++ Sources/RRColorKit/Extensions/String.swift
@@ -12,9 +12,10 @@ public extension String {
         var hexNumber: UInt64 = 0
         _ = Scanner(string: self).scanHexInt64(&hexNumber)
         
-        let red = CGFloat((hexNumber & 0xFF0000) >> 16) / 255
-        let green = CGFloat((hexNumber & 0x00FF00) >> 8) / 255
-        let blue = CGFloat((hexNumber & 0x0000FF) >> 0) / 255
+        let red = Double((hexNumber & 0xFF0000) >> 16) / 255
+        let green = Double((hexNumber & 0x00FF00) >> 8) / 255
+        let blue = Double((hexNumber & 0x0000FF) >> 0) / 255
+        
         return Color(red: red, green: green, blue: blue)
     }
 }
