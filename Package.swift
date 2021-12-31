c2fd800ffa218a499907885da7b649040e9282d4
Rudrank Riyam <rudrankriyam@gmail.com>
2022-01-01 02:03:43 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RRColorKit",
-    platforms: [.iOS(.v14), .macOS(.v11)],
+    platforms: [.iOS(.v15), .macOS(.v11)],
     products: [
         .library(
             name: "RRColorKit",
