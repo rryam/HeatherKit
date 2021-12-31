0d4306f04dd5ec5cacd125a86ab2bd87cdcfd39f
Rudrank Riyam <rudrankriyam@gmail.com>
2022-01-01 02:08:15 +0530

    Update RRColorKit to iOS 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RRColorKit",
-    platforms: [.iOS(.v15), .macOS(.v11)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "RRColorKit",
