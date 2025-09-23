3b3fb858aea1a5d9ab421abf346e9b35f51050cc
Rudrank Riyam <rudrankriyam@gmail.com>
2025-09-23 12:18:29 +0530

    Update GradientModel.swift

--- Sources/RRColorKit/Model/GradientModel.swift
+++ Sources/RRColorKit/Model/GradientModel.swift
@@ -40,8 +40,72 @@ public class RGBLogoGradient: RGBGradientProtocol {
 public class RGBTestGradient: RGBGradientProtocol {
     public var startColor: RGBColorProtocol
     public var endColor: RGBColorProtocol
-    
+
     public init(startColor: RGBColorProtocol = RGBTestStartColor(), endColor: RGBColorProtocol = RGBTestEndColor()) {
+        self.startColor = startColor
+        self.endColor = endColor
+    }
+}
+
+// MARK: - HSB Gradients
+
+public class HSBRandomGradient: HSBGradientProtocol {
+    public var startColor: HSBColorProtocol
+    public var endColor: HSBColorProtocol
+
+    public init(startColor: HSBColorProtocol = HSBRandomColor(), endColor: HSBColorProtocol = HSBRandomColor()) {
+        self.startColor = startColor
+        self.endColor = endColor
+    }
+}
+
+public class HSBUserGradient: HSBGradientProtocol {
+    public var startColor: HSBColorProtocol
+    public var endColor: HSBColorProtocol
+
+    public init(startColor: HSBColorProtocol = HSBInitialColor(), endColor: HSBColorProtocol = HSBInitialColor()) {
+        self.startColor = startColor
+        self.endColor = endColor
+    }
+}
+
+public class HSBTestGradient: HSBGradientProtocol {
+    public var startColor: HSBColorProtocol
+    public var endColor: HSBColorProtocol
+
+    public init(startColor: HSBColorProtocol = HSBTestColor(), endColor: HSBColorProtocol = HSBTestColor()) {
+        self.startColor = startColor
+        self.endColor = endColor
+    }
+}
+
+// MARK: - CMYK Gradients
+
+public class CMYKRandomGradient: CMYKGradientProtocol {
+    public var startColor: CMYKColorProtocol
+    public var endColor: CMYKColorProtocol
+
+    public init(startColor: CMYKColorProtocol = CMYKRandomColor(), endColor: CMYKColorProtocol = CMYKRandomColor()) {
+        self.startColor = startColor
+        self.endColor = endColor
+    }
+}
+
+public class CMYKUserGradient: CMYKGradientProtocol {
+    public var startColor: CMYKColorProtocol
+    public var endColor: CMYKColorProtocol
+
+    public init(startColor: CMYKColorProtocol = CMYKInitialColor(), endColor: CMYKColorProtocol = CMYKInitialColor()) {
+        self.startColor = startColor
+        self.endColor = endColor
+    }
+}
+
+public class CMYKTestGradient: CMYKGradientProtocol {
+    public var startColor: CMYKColorProtocol
+    public var endColor: CMYKColorProtocol
+
+    public init(startColor: CMYKColorProtocol = CMYKTestColor(), endColor: CMYKColorProtocol = CMYKTestColor()) {
         self.startColor = startColor
         self.endColor = endColor
     }
